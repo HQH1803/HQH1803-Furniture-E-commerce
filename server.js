738539fc8b66bd4845ce6f5294170f584b5751a9
Ho Quoc Huy <huyhuyhuy1803@gmail.com>
2025-04-21 04:04:29 +0700

    cap nhat server

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ app.use(cors({
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 if (!port) {
     console.error('PORT environment variable is required in production');
