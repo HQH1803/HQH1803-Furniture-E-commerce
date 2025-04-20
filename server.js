dcf0a28b8fa35a435f3ae7ca0f15bf15bd8df30d
Ho Quoc Huy <huyhuyhuy1803@gmail.com>
2025-04-20 19:00:37 +0700

    Thay doi trong file env

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ const app = express();
 const port = 4000;
 
 const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    port: "3307",
-    password: "",
-    database: "noithat",
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
 
 
 // Cấu hình multer để lưu trữ tệp tin
